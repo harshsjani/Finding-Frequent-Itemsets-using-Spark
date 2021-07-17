e74d7cf9f03f1fcd4b2fbc7db99d088d06f7e146
Harsh J <whizkid@gmail.com>
2021-07-16 18:46:42 -0700

    Cleaning the codebase. Adding a readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Finding Frequent Itemsets using Spark
+
+Implemented the SON algorithm to determine frequent itemsets in a subset of the Yelp academic dataset (reviews and businesses). This task was done for determining frequent sets of users who reviewed the same businesses as well as the other way around.
---
+++ scala/task2.scala
@@ -0,0 +1,214 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.storage.StorageLevel
+
+import java.io.{File, PrintWriter}
+import scala.collection.mutable.{ArrayBuffer, HashMap, HashSet}
+import scala.math.Ordering.Implicits._
+import scala.util.control.Breaks._
+
+object task2 {
+  val BUCKET_COUNT = 512428
+
+  def format_data(data: Array[List[String]]): String = {
+    var dtext = ""
+    var ml = 1
+
+    for (x <- data) {
+      val l = x.length
+
+      if (l > ml) {
+        dtext = dtext.stripSuffix(",")
+        dtext += "\n\n"
+        ml = l
+      }
+
+      dtext += "("
+
+      for (i <- x) {
+        dtext += "'" + i  + "'" + ", "
+      }
+
+      dtext = dtext.stripSuffix(", ")
+      dtext += "),"
+    }
+
+    return dtext.stripSuffix(",")
+  }
+
+  def write_data(output_file: String, cand: Array[List[String]], freq: Array[List[String]]) = {
+    var text = ""
+    text += "Candidates:\n"
+    text += format_data(cand)
+    text += "\n\n"
+    text += "Frequent Itemsets:\n"
+    text += format_data(freq).stripSuffix("\n\n")
+
+    val out = new PrintWriter(new File(output_file))
+    out.write(text)
+    out.close()
+  }
+
+  def get_L1(baskets: Array[Array[String]], s: Int): Set[String] = {
+    val counts = new HashMap[String, Int]().withDefaultValue(0)
+    var L1 = new HashSet[String]()
+
+    for (basket <- baskets) {
+      for (item <- basket) {
+        counts(item) += 1
+      }
+    }
+
+    for (item <- counts) {
+      if (item._2 >= s) {
+        L1 += item._1
+      }
+    }
+
+    return L1.toSet
+  }
+
+  def getCFromL(baskets: Array[Array[String]], L: Set[String], k: Int): Map[Set[String], Int] = {
+    val C = new HashMap[Set[String], Int]().withDefaultValue(0)
+
+    for (basket <- baskets) {
+      for (item <- basket.toSet.intersect(L).toArray.sorted.combinations(k)) {
+        C(item.toSet) += 1
+      }
+    }
+    return C.toMap
+  }
+
+  def getLFromC(C: Map[Set[String], Int], s: Int): Array[Array[String]] = {
+    val L = new ArrayBuffer[Array[String]]()
+
+    for (item <- C) {
+      if (item._2 >= s) {
+        L += item._1.toArray
+      }
+    }
+    return L.toArray
+  }
+
+  def filterBaskets(baskets: Array[Array[String]], freq_singles: Set[String]) : Array[Array[String]] = {
+    var newBaskets = new ArrayBuffer[Array[String]]()
+
+    for (basket <- baskets) {
+      newBaskets += basket.toSet.intersect(freq_singles).toArray
+    }
+    return newBaskets.toArray
+  }
+
+  def multihash(baskets: Array[Array[String]], s: Int, totalItems: Long): Iterator[Set[String]] = {
+    val support = math.ceil(s * baskets.length / totalItems.toFloat).toInt + 1
+    println("Support: " + support)
+
+    var k = 2
+
+    val l = get_L1(baskets, support)
+    var frequentCandidates = new ArrayBuffer[Set[String]]()
+
+    for (x <- l.toArray.sorted) {
+      frequentCandidates += HashSet(x).toSet
+    }
+
+    var temp = l
+
+    val baskets2 = filterBaskets(baskets, l)
+
+    breakable {
+      while (true) {
+        var ckp1 = getCFromL(baskets2, temp, k)
+        println("Processed C for k="+k)
+        var lkp1 = getLFromC(ckp1, support)
+        println("Processed L for k="+k)
+        ckp1 = null
+
+        if (lkp1.length == 0) {
+          break()
+        }
+
+        for (x <- lkp1) {
+          frequentCandidates += x.sorted.toSet
+        }
+
+        var t2 = new HashSet[String]()
+        for (x <- lkp1) {
+          t2 = t2.union(x.toSet)
+        }
+        lkp1 = null
+        temp = t2.toSet
+        k += 1
+      }
+    }
+    return frequentCandidates.toList.toIterator
+  }
+
+  def SONp2(baskets: Array[Array[String]], candidates: Array[Set[String]]): Iterator[Tuple2[Set[String], Int]] = {
+    val ctr = new HashMap[Set[String], Int]().withDefaultValue(0)
+
+    for (basket <- baskets) {
+      val bk = basket.toSet
+      for (candidate <- candidates) {
+        if (candidate.toArray.length > basket.length) {
+          // Figure this out later
+        }
+        if (candidate.subsetOf(bk)) {
+          ctr(candidate) += 1
+        }
+      }
+    }
+    val ret = ArrayBuffer[Tuple2[Set[String], Int]]()
+
+    for (item <- ctr) {
+      ret += Tuple2[Set[String], Int](item._1, item._2)
+    }
+    return ret.toArray.toIterator
+  }
+
+  def main(args: Array[String]): Unit = {
+    val sc = SparkContext.getOrCreate()
+    sc.setLogLevel("OFF")
+
+    val filter_threshold = args(0).toInt + 7
+    val support = args(1).toInt
+    val input_file = args(2)
+    val output_file = args(3)
+
+    val st = System.nanoTime
+
+    val textRDD = sc.textFile(input_file)
+    val first = textRDD.first()
+    val rdd = textRDD.filter(x => x != first)
+      .map(x => (x.split(",")(0), x.split(",")(1)))
+      .groupByKey()
+      .map(x => x._2.toSet.toArray)
+      .filter(x => (x.length > filter_threshold))
+
+    val totalItems = rdd.count()
+    rdd.unpersist(blocking = true)
+    // rdd.persist(StorageLevel.DISK_ONLY)
+
+    val candidates = rdd.mapPartitions(chunk => multihash(chunk.toArray, support, totalItems))
+      .distinct()
+      .sortBy(x => (x.toList.size, x.toList))
+      .collect()
+
+    println("Number of candidate sets: " + candidates.length)
+
+    val frequentItemsets = rdd.mapPartitions(chunk => SONp2(chunk.toArray, candidates))
+      .reduceByKey((x, y) => x + y)
+      .filter(x => x._2  >= support)
+      .map(x => x._1)
+      .distinct()
+      .map(x => x.toList.sorted)
+      .sortBy(x => (x.size, x))
+      .collect()
+
+    println("Number of frequent sets: " + frequentItemsets.length)
+    val cd = candidates.map(x => x.toList)
+    write_data(output_file, cd, frequentItemsets)
+
+    val et = (System.nanoTime - st) / 1e9d
+    println("Duration: " + et)
+  }
+}
